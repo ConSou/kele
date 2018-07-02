88ceb7cd74c07013b89fb516b2de84015cacb9ba
Conor Souhrada <ConorSouhrada@gmail.com>
2018-07-02 15:11:13 -0600

    checkpoint-2 commit

---
+++ lib/kele.rb
@@ -0,0 +1,11 @@
+require 'HTTParty'
+
+class Kele
+  include HTTParty
+
+  def initialize(email, password)
+    @base_url = 'https://www.bloc.io/api/v1'
+    @auth_token = self.class.post('https://www.bloc.io/api/v1/sessions', body: {email: email, password: password})
+  end
+
+end
