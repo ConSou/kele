3dfe3da1793f7eef76050e4ba22bc68f71a997b2
Conor Souhrada <ConorSouhrada@gmail.com>
2018-07-02 16:07:54 -0600

    checkpoint-3 commit

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,13 +1,20 @@
 require 'HTTParty'
+require 'json'
 
 class Kele
   include HTTParty
 
   def initialize(email, password)
     @base_url = 'https://www.bloc.io/api/v1'
-    
+
     response = self.class.post('https://www.bloc.io/api/v1/sessions', body: {email: email, password: password})
     @auth_token = response.parsed_response["auth_token"]
+  end
+
+  def get_me
+
+    response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: {"authorization" => @auth_token})
+    JSON.parse(response.body)
   end
 
 end
