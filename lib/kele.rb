ee3f0725bd2c02b0dd4343315c061886fcc70d81
Conor Souhrada <ConorSouhrada@gmail.com>
2018-07-03 10:52:06 -0600

    checkpoint-4 commit

--- lib/kele.rb
+++ lib/kele.rb
@@ -2,6 +2,7 @@ require 'HTTParty'
 require 'json'
 
 class Kele
+  attr_reader :user
   include HTTParty
 
   def initialize(email, password)
@@ -9,6 +10,7 @@ class Kele
 
     response = self.class.post('https://www.bloc.io/api/v1/sessions', body: {email: email, password: password})
     @auth_token = response.parsed_response["auth_token"]
+    @user = get_me
   end
 
   def get_me
@@ -17,8 +19,17 @@ class Kele
     JSON.parse(response.body)
   end
 
-  def get_mentor_availability(mentor_id)
-    response = self.class.get('https://www.bloc.io/api/v1/mentors/id/student_availability', headers: {"authorization" => @auth_token}, body: {id: mentor_id})
+  def get_mentor_availability()
+    unbooked_arr = []
+    mentor_id = @user["current_enrollment"]["mentor_id"]
+    response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: {"authorization" => @auth_token})
+    converted_response = JSON.parse(response.body)
+    converted_response.each do |element|
+      if element["booked"] == nil
+        unbooked_arr.push(element)
+      end
+    end
+    return unbooked_arr
   end
 
 
