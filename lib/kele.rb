d4bde463a53a27d0f08017df1b3c440f853d7d26
Conor Souhrada <ConorSouhrada@gmail.com>
2018-07-04 13:32:41 -0600

    Revisions

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,9 +5,14 @@ class Kele
 
   def initialize(email, password)
     @base_url = 'https://www.bloc.io/api/v1'
-    
+
     response = self.class.post('https://www.bloc.io/api/v1/sessions', body: {email: email, password: password})
-    @auth_token = response.parsed_response["auth_token"]
+    if response.parsed_response["auth_token"] == nil
+      puts "Error retreiving auth_token.  Please ensure email and password are correct."
+    else
+      @auth_token = response.parsed_response["auth_token"]
+    end
+
   end
 
 end
